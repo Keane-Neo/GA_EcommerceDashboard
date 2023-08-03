084f7dee6ae819868e9af478864096d6bf41f8a2
Keane <bkeaneneoweizhe@gmail.com>
2023-08-03 16:17:16 +0800

    feat: customers page with edit function

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,6 @@ import Customers from "./pages/Customers";
 
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [editRow, setEditRow] = useState({});
-  const [isAction, setIsAction] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [isDelete, setIsDelete] = useState(false);
-  const [customerData, setCustomerData] = useState([
-    {
-      id: "1",
-      name: "Keane",
-      email: "Keane@abc.com",
-      mobile: "999999",
-      joinDate: "01/01/2022",
-      orderCount: "4",
-    },
-  ]);
-
-  const resetButtons = () => {
-    setIsAction(false);
-    setIsEdit(false);
-    setIsDelete(false);
-  };
-  const handleEditClick = () => {
-    setIsAction(!isAction);
-    setIsEdit(!isEdit);
-  };
-
-  const handleDeleteClick = () => {
-    setIsAction(!isAction);
-    setIsDelete(!isDelete);
-  };
-
-  const handleConfirmClick = (id) => {
-    setIsAction(!isAction);
-    if (isDelete) {
-      const newData = customerData.filter((row) => row.id !== id);
-      setCustomerData(newData);
-      resetButtons();
-    } else if (isEdit) {
-      // check for inputs
-      resetButtons();
-    }
-  };
-
-  const handleCloseClick = () => {
-    resetButtons();
-  };
 
   const handleSidebarClick = () => setIsDrawerOpen(!isDrawerOpen);
 
@@ -56,14 +11,6 @@ function App() {
       <Customers
         isDrawerOpen={isDrawerOpen}
         handleSidebarClick={handleSidebarClick}
-        customerData={customerData}
-        editRow={editRow}
-        isAction={isAction}
-        handleEditClick={handleEditClick}
-        handleDeleteClick={handleDeleteClick}
-        handleConfirmClick={handleConfirmClick}
-        handleCloseClick={handleCloseClick}
-        isEdit={isEdit}
       />
     </>
   );
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -14,21 +14,97 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 
-const Customers = ({
-  isDrawerOpen,
-  handleSidebarClick,
-  editRow,
-  isAction,
-  handleEditClick,
-  handleDeleteClick,
-  handleConfirmClick,
-  handleCloseClick,
-  customerData,
-  isEdit,
-}) => {
+const Customers = ({ isDrawerOpen, handleSidebarClick }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [customerData, setCustomerData] = useState([
+    {
+      id: 1,
+      name: "Keane",
+      email: "Keane@abc.com",
+      mobile: "999999",
+      joinDate: "01/01/2022",
+      orderCount: "4",
+      isSelected: false,
+    },
+    {
+      id: 2,
+      name: "Keane",
+      email: "Keane@abc.com",
+      mobile: "999999",
+      joinDate: "01/01/2022",
+      orderCount: "4",
+      isSelected: false,
+    },
+  ]);
+  const resetButtons = () => {
+    setIsEdit(false);
+    setIsDelete(false);
+  };
+  const handleEditClick = (row) => {
+    updateIsSelectedTrue(row);
+    setIsEdit(!isEdit);
+  };
+
+  const handleDeleteClick = (row) => {
+    updateIsSelectedTrue(row);
+    setIsDelete(!isDelete);
+  };
+
+  const handleConfirmClick = (row) => {
+    // change back to edit / delete icon
+    updateIsSelectedFalse(row);
+
+    if (isDelete) {
+      const newData = customerData.filter((data) => row.id !== data.id);
+      setCustomerData(newData);
+      resetButtons();
+    } else if (isEdit) {
+      // check format of input
+
+      resetButtons();
+    }
+  };
+
+  const handleCloseClick = (row) => {
+    // change back to edit / delete icon
+    updateIsSelectedFalse(row);
+    resetButtons();
+  };
+
+  const updateIsSelectedFalse = (row) => {
+    const updatedCustomerData = customerData.map((data) => {
+      if (row.id === data.id) {
+        return { ...data, isSelected: false };
+      } else return data;
+    });
+    setCustomerData(updatedCustomerData);
+  };
+
+  const updateIsSelectedTrue = (row) => {
+    const updatedCustomerData = customerData.map((data) => {
+      if (row.id === data.id) {
+        return { ...data, isSelected: true };
+      } else return data;
+    });
+    setCustomerData(updatedCustomerData);
+  };
+
+  const handleChange = (row, e) => {
+    const updatedCustomerData = customerData.map((data) => {
+      if (row.id === data.id) {
+        return { ...data, [e.target.name]: e.target.value };
+      } else return data;
+    });
+    setCustomerData(updatedCustomerData);
+  };
+
+  const handleSubmit = () => {
+    console.log("submit");
+  };
   return (
     <Box>
       <Box
@@ -72,30 +148,59 @@ const Customers = ({
               <TableRow key={row.id}>
                 <TableCell>{row.id}</TableCell>
                 <TableCell>
-                  {isEdit ? (
-                    <TextField value={row.name} size="small"></TextField>
+                  {row.isSelected ? (
+                    <TextField
+                      name="name"
+                      type="text"
+                      onChange={(e) => handleChange(row, e)}
+                      value={row.name}
+                      size="small"
+                    ></TextField>
                   ) : (
                     row.name
                   )}
                 </TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.mobile}</TableCell>
+                <TableCell>
+                  {row.isSelected ? (
+                    <TextField
+                      name="email"
+                      type="email"
+                      onChange={(e) => handleChange(row, e)}
+                      value={row.email}
+                      size="small"
+                    ></TextField>
+                  ) : (
+                    row.email
+                  )}
+                </TableCell>
+                <TableCell>
+                  {row.isSelected ? (
+                    <TextField
+                      name="mobile"
+                      type="text"
+                      onChange={(e) => handleChange(row, e)}
+                      value={row.mobile}
+                      size="small"
+                    ></TextField>
+                  ) : (
+                    row.mobile
+                  )}
+                </TableCell>
                 <TableCell>{row.joinDate}</TableCell>
                 <TableCell>{row.orderCount}</TableCell>
                 <TableCell>
-                  {isAction ? (
+                  {row.isSelected ? (
                     <CheckIcon
-                      onClick={() => {
-                        handleConfirmClick(row.id);
-                      }}
+                      type="submit"
+                      onClick={() => handleConfirmClick(row)}
                     />
                   ) : (
-                    <EditIcon onClick={handleEditClick} />
+                    <EditIcon onClick={() => handleEditClick(row)} />
                   )}
-                  {isAction ? (
-                    <ClearIcon onClick={handleCloseClick} />
+                  {row.isSelected ? (
+                    <ClearIcon onClick={() => handleCloseClick(row)} />
                   ) : (
-                    <DeleteIcon onClick={handleDeleteClick} />
+                    <DeleteIcon onClick={() => handleDeleteClick(row)} />
                   )}
                 </TableCell>
               </TableRow>
